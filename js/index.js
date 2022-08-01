06909d74944e05ac510d9969baf53ac92b8def4e
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-08-01 14:19:01 -0500

    Create object books and listeners

--- js/index.js
+++ js/index.js
@@ -3,6 +3,23 @@ const title = document.getElementById('title');
 const authorName = document.getElementById('author');
 const addButton = document.getElementById('addBtn');
 
+const arrBooks = [];
 
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
 
+function addBook(title, author) {
+  const book = new Book(title, author);
+  arrBooks.push(book);
+  console.log(arrBooks);
+}
+
+addButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  addBook(title.value, authorName.value);
+  title.value = '';
+  authorName.value = '';
+});
 
