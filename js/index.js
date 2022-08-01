5ca6278156daaccf617568df4c016b493bb006ad
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-08-01 22:19:47 +0430

    add html design and js objects

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const booksContainer = document.getElementById('booksContainer');
+const title = document.getElementById('title');
+const authorName = document.getElementById('author');
+const addButton = document.getElementById('addBtn');
+
+
+
+
