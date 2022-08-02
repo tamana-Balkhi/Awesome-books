c3b94f3627f95790a6f8666f217a23fea284ccc2
Luis David Rodriguez <luisdavidrodriguezv@gmail.com>
2022-08-02 13:38:03 -0500

    Refactor classes with some errors

--- js/books.js
+++ js/books.js
@@ -1,68 +1,76 @@
 /* eslint-disable no-plusplus */
 import * as storage from './dataStorage.js';
 
+class BooksManager {
+  constructor(booksContainer) {
+    this.arrBooks = [];
+    this.booksContainer = booksContainer;
+  }
+
+  get getBooks() {
+    return this.arrBooks;
+  }
+
+  set setBooks(arr) {
+    this.arrBooks = [...arr];
+  }
+
+  Book(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  displayBooks() {
+    this.booksContainer.innerHTML = '';
+    for (let i = 0; i < this.arrBooks.length; i++) {
+      const book = this.arrBooks[i];
+      const bookBody = document.createElement('div');
+      const title = document.createElement('p');
+      const author = document.createElement('p');
+      const removeButton = document.createElement('button');
+
+      title.textContent = book.title;
+      author.textContent = book.author;
+      removeButton.id = `remove${i}`;
+      removeButton.textContent = 'remove';
+      removeButton.classList.add('remove');
+      bookBody.classList.add('bookdiv');
+      bookBody.append(title, author, removeButton);
+      this.booksContainer.append(bookBody);
+    }
+  }
+
+  addBook(title, author) {
+    this.arrBooks.push(new this.Book(title, author));
+    this.displayBooks();
+  }
+
+  removeBook(index) {
+    this.arrBooks = this.arrBooks.filter((_, position) => position !== index);
+    this.displayBooks();
+  }
+}
+
 const booksContainer = document.getElementById('booksContainer');
-const title = document.getElementById('title');
-const authorName = document.getElementById('author');
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
 const addButton = document.getElementById('addBtn');
 const storageAvailable = storage.storageAvailable();
-let arrBooks = [];
+const booksManager = new BooksManager(booksContainer);
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
-
-export default function displayBooks() {
-  if (storageAvailable) {
-    arrBooks = storage.getBooks();
-  }
-
-  booksContainer.innerHTML = '';
-  for (let i = 0; i < arrBooks.length; i++) {
-    const book = arrBooks[i];
-    const bookBody = document.createElement('div');
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const removeButton = document.createElement('button');
-
-    title.textContent = book.title;
-    author.textContent = book.author;
-    removeButton.id = `remove${i}`;
-    removeButton.textContent = 'remove';
-    removeButton.classList.add('remove');
-    bookBody.classList.add('bookdiv');
-    bookBody.append(title, author, removeButton);
-    booksContainer.append(bookBody);
-  }
-
-}
-
-function addBook(title, author) {
-  const book = new Book(title, author);
-  arrBooks.push(book);
-
-  if (storageAvailable) {
-    storage.setBooks(arrBooks);
-  }
-
-  displayBooks();
-}
-
-function removeBook(index) {
-  arrBooks = arrBooks.filter((_, position) => position !== index);
-  if (storageAvailable) {
-    storage.setBooks(arrBooks);
-  }
-
-  displayBooks();
+if (storageAvailable) {
+  booksManager.setBooks = storage.getBooks;
 }
 
 addButton.addEventListener('click', () => {
-  if (title.validity.valid && authorName.validity.valid) {
-    addBook(title.value, authorName.value);
-    title.value = '';
-    authorName.value = '';
+  if (inputTitle.validity.valid && inputAuthor.validity.valid) {
+    booksManager.addBook(inputTitle.value, inputAuthor.value);
+
+    if (storageAvailable) {
+      storage.setBooks(booksManager.getBooks());
+    }
+    inputTitle.value = '';
+    inputAuthor.value = '';
   }
 });
 
@@ -71,6 +79,13 @@ booksContainer.addEventListener('click', (event) => {
   const { id } = event.target;
   if (regex.test(id)) {
     const index = parseInt(id.match(regex)[0], 10);
-    removeBook(index);
+    booksManager.removeBook(index);
+    if (storageAvailable) {
+      storage.setBooks(booksManager.getBooks());
+    }
   }
 });
+
+export default function showBooks() {
+  booksContainer.displayBooks();
+}
